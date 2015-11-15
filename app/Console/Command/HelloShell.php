0a24d1f60dde7f508a1616b094e2ffdbbc2803a6
rasken2003 <kotobukijisan2003@gmail.com>
2015-11-15 23:14:46 +0900

    シェルサンプル

---
+++ app/Console/Command/HelloShell.php
@@ -0,0 +1,10 @@
+<?php
+class HelloShell extends AppShell {
+	public function main() {
+		$this->out('Hello world.');
+	}
+
+    public function hey_there() {
+        $this->out('Hey there ' . $this->args[0]);
+    }
+}
